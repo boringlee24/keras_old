71f4118c0ff85e781d492b8487cec67119edbeed
Baolin Li <baolin.li1994@gmail.com>
2020-08-11 00:49:18 -0400

    updated 3 schemes for simulator

--- examples/pwr_run/checkpointing/dash/simulator/opt_dash.py
+++ examples/pwr_run/checkpointing/dash/simulator/opt_dash.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cvxpy as cp
 
-
 def optimize_promotion(num_GPUs, job_num_GPUs, job_remaining_time):
 
     num_GPU_types = len(num_GPUs)
@@ -22,7 +21,7 @@ def optimize_promotion(num_GPUs, job_num_GPUs, job_remaining_time):
 
     x = cp.Variable(num_variables, boolean=True)
 
-    objective = cp.Maximize(remaining_times*x) #TODO
+    objective = cp.Minimize(remaining_times*x) #TODO
 
     constraints = []
     for j in range(num_jobs):
@@ -45,14 +44,43 @@ def optimize_promotion(num_GPUs, job_num_GPUs, job_remaining_time):
 
     return job_GPU_types
 
+def max_speedup_first(num_GPUs, job_num_GPUs, job_remaining_time):
+    job_speedup = {}
+    for job in job_remaining_time:
+        job_speedup[job] = job_remaining_time[job][0] / job_remaining_time[job][1]
+    num_V100 = num_GPUs[1]
+    num_jobs = len(job_num_GPUs)
+    sorted_pool = sorted(job_speedup, key=job_speedup.get, reverse=True)[:num_V100] 
+    jobs = list(job_num_GPUs.keys())
 
-#num_GPUs = [1, 1]  # K80, #P100, #V100 (order doesn't matter)
-#job_num_GPUs = {'1': 1, '3': 1}#, '4': 2, '17': 1, '30': 1}#,
+    job_GPU_types = {}
+    for i in range(num_jobs):
+        job_GPU_types[jobs[i]] = 1 if jobs[i] in sorted_pool else 0
+    return job_GPU_types
+
+
+def shortest_jobs_first(num_GPUs, job_num_GPUs, job_remaining_time):
+    v100_jrt = {}
+    for job in job_remaining_time:
+        v100_jrt[job] = job_remaining_time[job][1]
+    num_V100 = num_GPUs[1]
+    num_jobs = len(job_num_GPUs)
+    sorted_pool = sorted(v100_jrt, key=v100_jrt.get, reverse=False)[:num_V100]
+    jobs = list(job_num_GPUs.keys())
+
+    job_GPU_types = {}
+    for i in range(num_jobs):
+        job_GPU_types[jobs[i]] = 1 if jobs[i] in sorted_pool else 0
+    return job_GPU_types
+
+
+#num_GPUs = [3, 1]  # K80, #P100, #V100 (order doesn't matter)
+#job_num_GPUs = {'1': 1, '3': 1, '4': 1}#, '17': 1, '30': 1}#,
 ##                '45': 1, '60': 3}  # number of GPUs that the job is running on
-#job_remaining_time = {'1': [20000, 10000], '3': [3000, 1000]}#, '4': [500, 0], '17': [400, 300], '30': [
+#job_remaining_time = {'1': [20000, 10000], '3': [3000, 1000], '4': [500, 0]}#, '17': [400, 300], '30': [
 ##    100, 0]}#, '45': [400, 300], '60': [600, 500]}  # same order of runtimes as in 'num_GPUs'
 #
 ## GPU id in the same order as 'num_GPUs'
-#job_GPU_types = optimize_promotion(num_GPUs, job_num_GPUs, job_remaining_time)
+#job_GPU_types = shortest_jobs_first(num_GPUs, job_num_GPUs, job_remaining_time)
 #
 #print(job_GPU_types)
